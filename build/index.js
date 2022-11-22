85a04b328a8ea868c62cb180079a1e6c08c366bd
cgundy <cgundy@alumni.nd.edu>
2022-11-22 16:26:18 +0100

    create basic ts app

--- build/index.js
+++ build/index.js
@@ -1,3 +1,16 @@
 "use strict";
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var PORT = 3001;
+app.get('/', function (req, res) {
+    var num = 12;
+    res.json({ message: 'Welcome to Typescript Tutorial', number: num });
+    // also try showing typeof num
+});
+app.listen(PORT, function () {
+    console.log('server is running');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import express, {Request, Response} from 'express';
+
+const app = express();
+
+const PORT = 3001;
+
+app.get('/', (req: Request, res: Response): void => {
+
+    const num: Number = 12;
+
+    res.json({message: 'Welcome to Typescript Tutorial', number:  num});
+
+    // also try showing typeof num
+});
+
+app.listen(PORT, (): void => {
+    console.log('server is running')
+});
